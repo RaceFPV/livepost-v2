b331f528e8cb175185b95d9deabeefa33ba551ba
RaceFPV <racefpv@outlook.com>
2018-05-27 05:16:46 +0000

    clean up old non-working actioncable testing

--- app/channels/dashboard_channel.rb
+++
@@ -1,9 +0,0 @@
-class DashboardChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "dashboard_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
